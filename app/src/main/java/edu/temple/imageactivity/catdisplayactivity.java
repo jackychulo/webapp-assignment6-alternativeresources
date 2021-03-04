57ad7a3a4fbdd48b71debcf51ece4496533e6b7a
Jackychulo <jackyc1440gmail.com>
2021-03-04 00:10:44 -0500

    DisplayImageActivity can display name and image

--- app/src/main/java/edu/temple/imageactivity/catdisplayactivity.java
+++ app/src/main/java/edu/temple/imageactivity/catdisplayactivity.java
@@ -9,13 +9,23 @@ import android.widget.TextView;
 
 public class catdisplayactivity extends AppCompatActivity {
 
-
+    TextView textview;
+    ImageView imageview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display);
 
+        String catname = getIntent().getStringExtra("catname");
+        int catimageposition = getIntent().getIntExtra("catimageposition", 0);
 
+        textview = findViewById(R.id.textview);
+        imageview = findViewById(R.id.imageview);
+
+        textview.setText(catname);
+        textview.setGravity(Gravity.CENTER_HORIZONTAL);
+        textview.setTextSize(40);
+        imageview.setImageResource(catimageposition);
     }
 }
